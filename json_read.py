494ac7121cf0bc52e3be3235707cd7edb21f7921
ajitama <ideriver.chaos@gmail.com>
2018-12-07 19:13:01 +0900

    save

--- json_read.py
+++ json_read.py
@@ -15,9 +15,11 @@ def hello():
 @app.route('/json')
 def json_pp():
     file_json = "data/cities.json"
+    file_json = "data/event_json.json"
     fp_in = open (file_json,encoding='utf-8')
     json_str = fp_in.read()
     fp_in.close()
+    dict_aa = {}
     dict_aa = json.loads(json_str)
 #
     rows = []
@@ -26,7 +28,7 @@ def json_pp():
         unit['id'] = key
         rows.append(unit)
 #
-    return render_template('main.html', title=file_json, cities=rows)
+    return render_template('main.html', title=file_json, json_rows=rows)
 
 if __name__ == "__main__":
     app.run(debug=True)
